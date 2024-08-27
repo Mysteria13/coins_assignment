025aa6459e173a8ea06f2ea79ff6021979d0db4a
Mysteria13 <lisaeseohenohimai@gmail.com>
2024-08-27 13:52:45 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("coin_assignment")
+st.write(name = st.text_input('Please enter your name')
+
