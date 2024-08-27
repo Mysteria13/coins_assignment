a0e7f3ceb6f6b46930ec5130efcb49eb1e6a6cb3
Mysteria13 <lisaeseohenohimai@gmail.com>
2024-08-27 14:02:06 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,4 +2,7 @@ import streamlit as st
 
 st.title("coin_assignment")
 st.write (name = st.text_input('Please enter your name'))
-
+st.write (gold_coins = st.text_input('Please enter your amount of gold coins'))
+st.write (silver_coins = st.text_input('Please enter your amount of silver coins'))
+st.write (bronze_coins = st.text_input('Please enter your amount of bronze coins'))
+st.write(total_coins = gold_coins + silver_coins + bronze_coins)
